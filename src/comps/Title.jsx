058e88f4dc351727d00960ac516bb8d6d98b40f4
Anush Poudel <apoudel55@gmail.com>
2021-04-07 14:16:27 +1000

    final prototype

---
+++ src/comps/Title.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Title = () => {
+  return (
+    <div className="title">
+      <h1>FireGram</h1>
+      <h2>Your Pictures</h2>
+      <p>A simple photo gallery app powered by react and firebase.</p>
+    </div>
+  );
+};
+
+export default Title;
---
+++ src/firebase/config.js
@@ -0,0 +1,21 @@
+import firebase from "firebase/app";
+import "firebase/storage";
+import "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: "firegram-react-aa8fa.firebaseapp.com",
+  projectId: "firegram-react-aa8fa",
+  storageBucket: "firegram-react-aa8fa.appspot.com",
+  messagingSenderId: "925592814370",
+  appId: "1:925592814370:web:2b986b309e2a6a07ef5b7b",
+  measurementId: "G-1353SCH64W",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const projectStorage = firebase.storage();
+const projectFirestore = firebase.firestore();
+const timestamp = firebase.firestore.FieldValue.serverTimestamp;
+
+export { projectStorage, projectFirestore, timestamp };
